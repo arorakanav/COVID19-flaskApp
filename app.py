863c054fa5be69a39ca4fc5edffdddfafb3c006e
Kanav Arora <arorakanav11@gmail.com>
2020-04-14 17:05:13 -0400

    [TASK] Fixed scheduler

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 
 from src.covidapp import covidapp
-from src.update_covid_data import (update_ccse_covid_time_series_confirmed_global,
-  update_ccse_covid_time_series_recovered_global,
-  update_ccse_covid_time_series_deaths_global)
-
-from apscheduler.schedulers.background import BackgroundScheduler
 
 
 #Creating a FLASK instance
@@ -16,9 +11,6 @@ CORS(app)
 
 @app.route("/")
 def helloWorld():
-  # update_ccse_covid_time_series_confirmed_global()
-  # update_ccse_covid_time_series_deaths_global()
-  # update_ccse_covid_time_series_recovered_global()
   return "Hello, cross-origin-world!"
 
 #Cors
@@ -29,11 +21,6 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(update_ccse_covid_time_series_confirmed_global, 'interval', minutes=20)
-# scheduler.add_job(update_ccse_covid_time_series_recovered_global, 'interval', minutes=40)
-# scheduler.add_job(update_ccse_covid_time_series_deaths_global, 'interval', minutes=60)
-scheduler.start()
 if __name__ == '__main__':
     app.run(threaded=True, host="0.0.0.0", use_reloader=True, debug=True)
     
---
+++ src/clock.py
@@ -0,0 +1,14 @@
+from src.update_covid_data import (update_ccse_covid_time_series_confirmed_global,
+  update_ccse_covid_time_series_recovered_global,
+  update_ccse_covid_time_series_deaths_global)
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+scheduler = BackgroundScheduler()
+@scheduler.scheduled_job('interval', minutes=20)
+def update_ccse_covid_time_series_global_job():
+  update_ccse_covid_time_series_confirmed_global()
+  update_ccse_covid_time_series_recovered_global()
+  update_ccse_covid_time_series_deaths_global()
+
+scheduler.start()
