dbfb1f8ff5eb7f97f8be1879fe161a87edb0be02
Kanav Arora <arorakanav11@gmail.com>
2020-04-15 22:51:01 -0400

    [FIX] bacjground scheduler import

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from src.update_covid_data import (update_ccse_covid_time_series_confirmed_globa
   update_ccse_covid_time_series_recovered_global,
   update_ccse_covid_time_series_deaths_global)
 
-from apscheduler.schedulers.blocking import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 @scheduler.scheduled_job('interval', minutes=20)
