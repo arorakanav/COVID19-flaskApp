11478f2474b9d8b29e474a50069871170b2446dd
Kanav Arora <arorakanav11@gmail.com>
2020-04-16 08:16:47 -0400

    [WIP] working on scheduler

--- clock.py
+++ clock.py
@@ -5,15 +5,15 @@ from src.update_covid_data import (update_ccse_covid_time_series_confirmed_globa
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
-@scheduler.scheduled_job('interval', minutes=20)
+@scheduler.scheduled_job('interval', minutes=60)
 def update_ccse_covid_time_series_confirmed_global_job():
   update_ccse_covid_time_series_confirmed_global()
 
-@scheduler.scheduled_job('interval', minutes=20)
+@scheduler.scheduled_job('interval', minutes=60)
 def update_ccse_covid_time_series_recovered_global_job():
   update_ccse_covid_time_series_recovered_global()
 
-@scheduler.scheduled_job('interval', minutes=20)
+@scheduler.scheduled_job('interval', minutes=60)
 def update_ccse_covid_time_series_deaths_global_job():
   update_ccse_covid_time_series_deaths_global()
 
